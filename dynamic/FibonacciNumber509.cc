4899666f3946441d86732ac4d34705078c8caad8
刘名卓 <88214924@qq.com>
2020-11-21 18:47:20 +0800

    fibonacci number

--- dynamic/FibonacciNumber509.cc
+++ dynamic/FibonacciNumber509.cc
@@ -15,25 +15,34 @@
 using namespace std;
 class Solution {
 public:
-    int fib(int N) {
-        if (N < 1) return 0;
-        //创建一个备忘录，存储计算过的值，减少时间复杂度
-        vector<int>temp(N+1,0);
-        return decorator(temp,N);
+    // int fib(int N) {
+    //     if (N < 1) return 0;
+    //     //创建一个备忘录，存储计算过的值，减少时间复杂度
+    //     vector<int>temp(N+1,0);
+    //     return decorator(temp,N);
+    // }
+    // int decorator(vector<int> &temp,int n){
+    //     if (n ==1 || n==2) return 1;
+    //     if (temp[n] != 0)  return temp[n];
+    //     temp[n] = decorator(temp,n-1) + decorator(temp,n-2);
+    //     return temp[n];
+    // }   
+    
+    int fib(int N){ //迭代解法
+        if (N ==1 || N ==2) return 1;
+        int prev = 1,cur = 1;
+        for(int i =3; i<=N ;i++){
+            int sum = prev + cur;
+            prev = cur;
+            cur = sum;
+        }
+        return cur;
     }
-    int decorator(vector<int> &temp,int n){
-        if (n ==1 || n==2) return 1;
-        if (temp[n] != 0)  return temp[n];
-        temp[n] = decorator(temp,n-1) + decorator(temp,n-2);
-        return temp[n];
-    }   
- 
-
 };
 
 int main(){
     Solution s = Solution();
-    int fibRlt = s.fib(2);
+    int fibRlt = s.fib(3);
     cout <<fibRlt <<endl;
 
 }
