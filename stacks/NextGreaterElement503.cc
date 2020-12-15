14d32bdc8d0d99d2b952b2c805a7e4af90a8c1fc
刘名卓 <88214924@qq.com>
2020-12-15 23:57:50 +0800

    stacks

---
+++ stacks/NextGreaterElement503.cc
@@ -0,0 +1,25 @@
+/**
+503. 下一个更大元素 II
+给定一个循环数组（最后一个元素的下一个元素是数组的第一个元素），输出每个元素的下一个更大元素。数字 x 的下一个更大的元素是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1。
+
+输入: [1,2,1]
+输出: [2,-1,2]
+解释: 第一个 1 的下一个更大的数是 2；
+数字 2 找不到下一个更大的数； 
+第二个 1 的下一个最大的数需要循环搜索，结果也是 2。
+
+ */
+#include<vector>
+#include<stack>
+using namespace std;
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> res;
+        stack<int> stacks;
+        for(auto num:nums){
+            while (!stacks.empty() && stacks.top() <= num) stacks.pop();
+            res.push_back(stacks.empty()?-1:stacks.top());            
+        }
+    }
+};
