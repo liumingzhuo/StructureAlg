fdf298205b2a5571f07b2b3987062d9320ea52b2
刘名卓 <88214924@qq.com>
2021-01-11 00:33:06 +0800

    fib

--- dynamic/FibonacciNumber509/FibonacciNumber509.go
+++ dynamic/FibonacciNumber509/FibonacciNumber509.go
@@ -1,21 +1,58 @@
 /*
 509. Fibonacci Number
 */
-package FibonacciNumber509
+package main
 
+//方法1  备忘录
 func fib(n int) int {
 	if n < 1 {
 		return 0
 	}
+	//创建一个备忘录
+	memo := make([]int, n+1)
+	return helper(memo, n)
+}
+func helper(memo []int, n int) int {
 	if n == 1 || n == 2 {
 		return 1
 	}
-	prev := 1
+	if memo[n] != 0 {
+		return memo[n]
+	}
+	memo[n] = helper(memo, n-1) + helper(memo, n-2)
+	return memo[n]
+}
+
+//自底向上 dp
+func fib1(n int) int {
+	if n < 1 {
+		return 0
+	}
+	dp := make([]int, n+1)
+	if n == 1 || n == 2 {
+		return 1
+	}
+	dp[1] = dp [2] = 1 
+	for i := 3; i <= n; i++{
+		dp[i] = dp[i-1]+dp[i-2]
+	}
+	return dp[n]
+}
+
+//优化自底向上的dp
+func fib2(n int) int{
+	if n < 1 {
+		return 0
+	}
+	if n == 1|| n ==2{
+		return 1
+	}
 	curr := 1
-	for i = 3; i <= n; i++ {
-		sum := prev + curr
-		prev = curr
-		curr = sum
+	prev := 1
+	for i := 3; i <= n; i++{
+		sum := curr + prev
+        prev = curr
+        curr = sum
 	}
 	return curr
 }
