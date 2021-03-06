1019cf06e3e1059b28d219613b9d945769684f46
刘名卓 <88214924@qq.com>
2021-03-06 16:51:38 +0800

    invert tree

---
+++ binaryTree/inverttree226/invertTree226.go
@@ -0,0 +1,20 @@
+package inverttree226
+
+//226. 翻转二叉树
+
+//TreeNode 结点
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
